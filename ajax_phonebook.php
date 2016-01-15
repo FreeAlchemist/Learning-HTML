f893a5d478f3f36721d75ff3a243c207d8c1c34c
FreeAlchemist <freealchemist@mail.ru>
2016-01-15 16:48:50 +0300

    Update ajax_phonebook.php

--- ajax_phonebook.php
+++ ajax_phonebook.php
@@ -3,6 +3,7 @@
 <head>
 	<title>ajax_showfile</title>
 	<meta charset="utf-8">
+<link rel=Stylesheet href="ajax_phonebook.css">
 	<script type="text/javascript" src="phone_request.js"></script>
 	<script type="text/javascript" src="jquery/jquery-1.11.3.js"></script>
 	<?php
@@ -32,5 +33,57 @@ foreach ($arr as $key => $value) {
 <form>
 	<input type='submit' name="download" value="download">
 </form>
+
+
+	
+<?php
+
+$encoding=iconv_get_encoding('all');
+print_r($encoding);
+
+$filename="10.xml";
+if (file_exists($filename)) {
+    $xml = simplexml_load_file($filename);
+    // print_r($xml);
+}
+else {
+    exit('Failed to open '.$filename);
+}
+echo '<form method="get" action="10form.php">';
+echo '<input type="submit" value="Добавить контакт">';
+echo '<input type="hidden" name="filename" value="'.$filename.'">';
+echo '<input type="hidden" name="add" value="1">';
+echo '</form>';
+echo "<table rules=\"all\">";
+	echo "<tr>
+		<td>ФИО</td>
+		<td>Телефон</td>
+		<td>Дата рождения</td>
+		<td>Адрес</td>
+		</tr>";
+foreach ($xml as $key => $value) {
+	echo "<tr>";
+	echo "<td>";
+	echo "<a href=\"10form.php?id=".$value['id']."&filename=".$filename."\">"
+	    .iconv("UTF-8","cp1251",$value->fio->lastname)." "
+	    .iconv("UTF-8","cp1251",$value->fio->firstname)." "
+	    .iconv("UTF-8","cp1251",$value->fio->surname)."</a>";
+	echo "</td>";
+	echo "<td>";
+	echo $value->phone;
+	echo "</td>";
+	echo "<td>";
+	echo iconv("UTF-8","cp1251",$value->birthdate->day)."."
+		.iconv("UTF-8","cp1251",$value->birthdate->month)."."
+		.iconv("UTF-8","cp1251",$value->birthdate->year);
+	echo "</td>";
+	echo "<td>";
+	echo iconv("UTF-8","cp1251",$value->adress->country).", "
+		.iconv("UTF-8","cp1251",$value->adress->city);
+	echo "</td>";
+	echo "</tr>";
+}
+echo "</table>";
+?>
 </body>
 </html>
