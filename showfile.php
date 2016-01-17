31458524e5c75bfeed15c0615637e58a3c609b5e
FreeAlchemist <freealchemist@mail.ru>
2016-01-18 00:33:19 +0300

    Update showfile.php

--- showfile.php
+++ showfile.php
@@ -6,7 +6,6 @@
 	<script type="text/javascript" src="request.js"></script>
 	<?php
 	include("showdir.php");
-	// showpath("uploads");
 	print_r(showpath("uploads"));
 	?>
 </head>
@@ -14,9 +13,7 @@
 <?php
 $arr=showpath("uploads");
 foreach ($arr as $key => $value) {
-
 	echo "<li><span onclick=\"http_zapros('GET','uploads/".$value."',obrabotka)\">".$value."</span>"."<a href=\"uploads/".$value."\"> скачать</a></li>";
-
 }
 ?>
 </ul>
