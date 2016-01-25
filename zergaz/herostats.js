c98b6ef04bf01996e2dcad3b8ff520a974a917ce
FreeAlchemist <freealchemist@mail.ru>
2016-01-25 19:10:17 +0300

    Update herostats.js

--- zergaz/herostats.js
+++ zergaz/herostats.js
@@ -1,28 +1,22 @@
-// HEALTH, H_MOD, H_REGEN, H_REGEN_MOD, MANA, M_REGEN, ATK_SPD, DAMAGE, DAMAGE_MOD, MOVE_SPD
-var zer = {"NAME":"Zeratul", "HEALTH":1607,"H_MOD":65,"H_REGEN":3.35,"H_REGEN_MOD":0.13,"MANA":500,"M_REGEN":3,"ATK_SPD":0.8,"DAMAGE":37,"DAMAGE_MOD":13,"MOVE_SPD":4.4}
-var gaz = {"NAME":"Gazlowe", "HEALTH":1900,"H_MOD":76,"H_REGEN":3.95,"H_REGEN_MOD":0.16,"MANA":500,"M_REGEN":3,"ATK_SPD":0.9,"DAMAGE":47,"DAMAGE_MOD":9,"MOVE_SPD":4.4}
+// HEALTH, hpMod, H_REGEN, hpRegenMod, MANA, M_REGEN, ATK_SPD, DAMAGE, DAMAGE_MOD, MOVE_SPD
+var zer = {"NAME":"Zeratul", "startHP":1607,"hpMod":65,"H_REGEN":3.35,"hpRegenMod":0.13,"MANA":500,"M_REGEN":3,"ATK_SPD":0.8,"DAMAGE":37,"DAMAGE_MOD":13,"MOVE_SPD":4.4, "level":5}
+var gaz = {"NAME":"Gazlowe", "startHP":1900,"hpMod":76,"H_REGEN":3.95,"hpRegenMod":0.16,"MANA":500,"M_REGEN":3,"ATK_SPD":0.9,"DAMAGE":47,"DAMAGE_MOD":9,"MOVE_SPD":4.4, "level":5}
 
-// function Hero(NAME, HEALTH, H_MOD, H_REGEN, H_REGEN_MOD, MANA, M_REGEN, ATK_SPD, DAMAGE, DAMAGE_MOD, MOVE_SPD){
-// 		this.NAME = NAME
-// 		this.HEALTH = HEALTH
-// 		this.H_MOD = H_MOD
-// 		this.H_REGEN = H_REGEN
-// 		this.H_REGEN_MOD = H_REGEN_MOD
-// 		this.MANA = MANA
-// 		this.M_REGEN = M_REGEN
-// 		this.ATK_SPD = ATK_SPD
-// 		this.DAMAGE = DAMAGE
-// 		this.DAMAGE_MOD = DAMAGE_MOD
-// 		this.MOVE_SPD = MOVE_SPD || 4.4
-// 	}
 
 function Hero(obj){
 	console.log(obj)
+	this.def = obj
+	this.level = obj.level
+	console.log(this)
+	this.abilities = {}
+	this.addAutoAtk()
+	this.maxHP = this.getMaxHP()
+
 	this.NAME = obj.NAME
-	this.HEALTH = obj.HEALTH
-	this.H_MOD = obj.H_MOD
+	this.startHP = obj.startHP
+	this.hpMod = obj.hpMod
 	this.H_REGEN = obj.H_REGEN
-	this.H_REGEN_MOD = obj.H_REGEN_MOD
+	this.hpRegenMod = obj.hpRegenMod
 	this.MANA = obj.MANA
 	this.M_REGEN = obj.M_REGEN
 	this.ATK_SPD = obj.ATK_SPD
@@ -31,13 +25,29 @@ function Hero(obj){
 	this.MOVE_SPD = obj.MOVE_SPD || 4.4
 }
 
-Hero.prototype.beat = function(level){
-		return (this.DAMAGE + this.DAMAGE_MOD * level) * this.ATK_SPD
+Hero.prototype.beat = function(){
+		return (this.DAMAGE + this.DAMAGE_MOD * this.level) * this.ATK_SPD
 	}
 
-Hero.prototype.regen = function(level){
-		return this.H_REGEN + this.H_REGEN_MOD * level
+Hero.prototype.regen = function(){
+		return this.H_REGEN + this.hpRegenMod * this.level
 	}
+
+Hero.prototype.getMaxHP = function(){
+	return this.def.startHP + this.def.hpMod * this.level
+}
+
+Hero.prototype.addAutoAtk = function(){
+	this.abilities.autoattack = {
+		func : this.beat(),
+		cooldown : this.getCooldown()
+	}
+}
+
+Hero.prototype.getCooldown = function(){
+	return 1/this.ATK_SPD
+}
+
 
 function Zeratul(obj){
 	// Hero.call(this, obj)
@@ -48,9 +58,14 @@ Zeratul.prototype = Object.create(Hero.prototype)
 Zeratul.prototype.constructor = Zeratul
 
 
+
+
+
+
 // var gaz = {name:1}
 
 zeratul = new Zeratul(zer)
+
 // zeratul = new Zeratul("Zeratul", 1607, 65, 3.35, 0.13, 500, 3, 0.8, 37, 13)
 // zeratul = new Hero("Zeratul", 1607, 65, 3.35, 0.13, 500, 3, 0.8, 37, 13)
 
