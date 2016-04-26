e02c44e74d7b8250860ac260b094086614f1019f
FreeAlchemist <freealchemist@mail.ru>
2016-04-26 17:42:45 +0300

    Update cardsCreate.js

--- pursuits/js/cardsCreate.js
+++ pursuits/js/cardsCreate.js
@@ -23,22 +23,25 @@ for (var i = 0; i < quantity; i++) {
 	var cardbackid = cardid+'-back';
 	console.log(cardid)
 	$('#page').append($('<div />',{class:'card card-front',id:cardfrontid}))
-	$('#'+cardfrontid).append($('<div />',{class:'front',id:cardfrontid+'-bg'}))
-	$('#'+cardfrontid+'-bg').append($('<div />',{class:'title pursuit-title',text:pursuit[cardid].name}))
-	$('#'+cardfrontid+'-bg').append($('<div />',{class:'text pursuit-text',text:pursuit[cardid].text}))
+	$('#'+cardfrontid).append($('<div />',{class:'front1',id:cardfrontid+'-bg1'}))
+	$('#'+cardfrontid).append($('<div />',{class:'front2',id:cardfrontid+'-bg2'}))
 
-	var gearid = cardfrontid+'-bg'+'-gear'
-	$('#'+cardfrontid+'-bg').append($('<table />',{class:'text step',id:gearid,cellspacing:'0',cellpadding:'2'}))
+
+	$('#'+cardfrontid+'-bg1').append($('<div />',{class:'title pursuit-title',text:pursuit[cardid].name}))
+	$('#'+cardfrontid+'-bg1').append($('<div />',{class:'text pursuit-text',text:pursuit[cardid].text}))
+
+	var gearid = cardfrontid+'-bg1'+'-gear'
+	$('#'+cardfrontid+'-bg1').append($('<table />',{class:'text gear step',id:gearid,cellspacing:'0',cellpadding:'2'}))
 	var gearrowid = gearid+'-row'
 	$('#'+gearid).append($('<tr />',{id:gearrowid}))
 	$('#'+gearrowid).append($('<td />',{text:'GEAR: '}))
 	$('#'+gearrowid).append($('<td />',{text:pursuit[cardid].gear}))
 
-	$('#'+cardfrontid+'-bg').append($('<div />',{class:'title',text:pursuit[cardid].talent['name']}))
-	$('#'+cardfrontid+'-bg').append($('<div />',{class:'text talent-text',text:pursuit[cardid].talent['text']}))
+	$('#'+cardfrontid+'-bg1').append($('<div />',{class:'title',text:pursuit[cardid].talent['name']}))
+	$('#'+cardfrontid+'-bg1').append($('<div />',{class:'text talent-text',text:pursuit[cardid].talent['text']}))
 
-	var stepid = cardfrontid+'-bg'+'-step'
-	$('#'+cardfrontid+'-bg').append($('<table />',{class:'text step',id:stepid,cellspacing:'0',cellpadding:'0'}))
+	var stepid = cardfrontid+'-bg2'+'-step'
+	$('#'+cardfrontid+'-bg2').append($('<table />',{class:'text step',id:stepid,cellspacing:'0',cellpadding:'0'}))
 	$('#'+stepid).append($('<tr />',{id:stepid+'-row-0'}))
 	$('#'+stepid+'-row-0').append($('<td />',{class:'step-title',text:'Step'}))
 	$('#'+stepid+'-row-0').append($('<td />',{class:'step-title',text:'Talent'}))
