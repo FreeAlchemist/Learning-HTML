5fb7a07f4f2682fcd0e5916c8082e8a5d1a173f1
FreeAlchemist <freealchemist@mail.ru>
2016-04-27 07:41:58 +0300

    Create pursuits-under-quarantine.js

---
+++ pursuits/js/pursuits-under-quarantine.js
@@ -0,0 +1,69 @@
+var pursuit = {
+	'bully' : {
+		type:'Under Quarantine',
+		name: 'bully',
+		text:'bully text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...bully'
+		},
+		step:{
+			'1' : 'bully step1',
+			'2' : 'General Talent',
+			'3' : 'bully step3',
+			'4' : 'General Talent',
+			'5' : 'bully step5',
+			'6' : 'General Talent',
+			'7' : 'bully step7',
+			'8' : 'General Talent',
+			'9' : 'bully step9',
+			'10' : 'bully step10',
+		}
+	},
+	
+	'ghost-eater' : {
+		type:'Under Quarantine',
+		name: 'ghost-eater',
+		text:'ghost-eater text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...ghost-eater'
+		},
+		step:{
+			'1' : 'ghost-eater step1',
+			'2' : 'General Talent',
+			'3' : 'ghost-eater step3',
+			'4' : 'General Talent',
+			'5' : 'ghost-eater step5',
+			'6' : 'General Talent',
+			'7' : 'ghost-eater step7',
+			'8' : 'General Talent',
+			'9' : 'ghost-eater step9',
+			'10' : 'ghost-eater step10',
+		}
+	},
+	'ghoul' : {
+		type:'Under Quarantine',
+		name: 'ghoul',
+		text:'ghoul text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...ghoul'
+		},
+		step:{
+			'1' : 'ghoul step1',
+			'2' : 'General Talent',
+			'3' : 'ghoul step3',
+			'4' : 'General Talent',
+			'5' : 'ghoul step5',
+			'6' : 'General Talent',
+			'7' : 'ghoul step7',
+			'8' : 'General Talent',
+			'9' : 'ghoul step9',
+			'10' : 'ghoul step10',
+		}
+	},
+}
