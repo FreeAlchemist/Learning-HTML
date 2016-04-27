9560179f95c6dafcf659e07681c9b0cd9023d45a
FreeAlchemist <freealchemist@mail.ru>
2016-04-27 17:13:36 +0300

    Update pursuits-advanced.js

--- pursuits/js/pursuits-advanced.js
+++ pursuits/js/pursuits-advanced.js
@@ -1,6 +1,7 @@
 var pursuit = {
+	type:'Advanced',
+	faction:'advanced',
 	'death-marshal' : {
-		type:'Advanced',
 		name: 'death-marshal',
 		text:'death-marshal text',
 		gear:'',
@@ -9,16 +10,11 @@ var pursuit = {
 			text:'Whenever this character...death-marshal'
 		},
 		step:{
-			'1' : 'death-marshal step1',
-			'2' : 'General Talent',
-			'3' : 'death-marshal step3',
-			'4' : 'General Talent',
-			'5' : 'death-marshal step5',
-			'6' : 'General Talent',
-			'7' : 'death-marshal step7',
-			'8' : 'General Talent',
-			'9' : 'death-marshal step9',
-			'10' : 'death-marshal step10',
+			'1' : 'Peacebringer',
+			'2' : 'Coffin',
+			'3' : 'Special Dispensation',
+			'4' : 'Pine Box',
+			'5' : 'Flaming Head',
 		}
 	},
 }
