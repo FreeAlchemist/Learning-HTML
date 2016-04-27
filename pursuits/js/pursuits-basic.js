57404b397146d5756a6232e3fac592bfc871f49b
FreeAlchemist <freealchemist@mail.ru>
2016-04-27 17:14:47 +0300

    Update pursuits-basic.js

--- pursuits/js/pursuits-basic.js
+++ pursuits/js/pursuits-basic.js
@@ -1,12 +1,13 @@
 var pursuit = {
+	type:'Basic',
+	faction:'basic',
 	'academic' : {
-		type:'Basic',
 		name:'academic',
 		text:'Academics are valued for the knowledge they possess and can contribute. They tend to have heightened familiarity with Malifaux culture and architecture, providing essential information to help navigate those lost constructs and find both information and items of value and power.',
 		gear:'non-magical skill toolkit (pg 179)',
 		talent:{
 			name:'avid Student',
-			text:'Whenever this character fails an Academic duel he may draw a card. During the Epilogue, a character on this Pursuit may advance in any Academic skill in addition to those skill Advancement options predented by the Fatemaster.'
+			text:'Whenever failed an Academic duel, may draw a card. Epilogue: may advance in any Academic skill in addition to skill Advancement options.'
 		},
 		step:{
 			'1' : 'Student of Knowledge',
@@ -22,289 +23,276 @@ var pursuit = {
 		}
 	},
 	'criminal' : {
-		type:'Basic',
 		name: 'criminal',
 		text:'criminal text',
 		gear:'non-magical skill toolkit (pg 179)',
 		talent:{
 			name:'opportunist',
-			text:'Whenever this character...criminal'
+			text:'Whenever failed an Expertise duel, may draw a card. Epilogue: may advance in any Expertise skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'criminal step1',
+			'1' : 'Competitive Edge',
 			'2' : 'General Talent',
-			'3' : 'criminal step3',
+			'3' : 'Improvise or Quick Work',
 			'4' : 'General Talent',
-			'5' : 'criminal step5',
+			'5' : 'Improvise or Nimble',
 			'6' : 'General Talent',
-			'7' : 'criminal step7',
+			'7' : 'Quick Work or Surprise Assault',
 			'8' : 'General Talent',
-			'9' : 'criminal step9',
-			'10' : 'criminal step10',
+			'9' : 'Surprise Assault or Nimble',
+			'10' : 'Master of Misdirection',
 		}
 	},
 	'dabbler' : {
-		type:'Basic',
 		name: 'dabbler',
 		text:'dabbler text',
 		gear:'Grimoire: 1 Sorcery Magia, 1 Prestidigitation Magia, 3 Immuto. Magical Theory',
 		talent:{
 			name:'epiphany',
-			text:'Whenever this character...dabbler'
+			text:'When failed a Magic duel during dramatic time, may draw a card. Epilogue: may advance in any Magical skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'dabbler step1',
+			'1' : 'Arcane Musings',
 			'2' : 'General Talent',
-			'3' : 'dabbler step3',
+			'3' : 'Mastered Immuto or Spell Affinity',
 			'4' : 'General Talent',
-			'5' : 'dabbler step5',
+			'5' : 'Mastered Magia or Spell Affinity',
 			'6' : 'General Talent',
-			'7' : 'dabbler step7',
+			'7' : 'Mastered Immuto or Mastered Magia',
 			'8' : 'General Talent',
-			'9' : 'dabbler step9',
-			'10' : 'dabbler step10',
+			'9' : 'Mastered Magia or Spell Affinity',
+			'10' : 'Empowered Channeling',
 		}
 	},
 	'drudge' : {
-		type:'Basic',
 		name: 'drudge',
 		text:'drudge text',
 		gear:'1 pneumatic limb + steam augments (total value up to 25§)',
 		talent:{
 			name:'hard days night',
-			text:'Whenever this character...drudge'
+			text:'Whenever failed a Training duel, may draw a card. Epilogue: may advance in any Training skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'drudge step1',
+			'1' : 'Long Days',
 			'2' : 'General Talent',
-			'3' : 'drudge step3',
+			'3' : 'Steady Pace or Team Work',
 			'4' : 'General Talent',
-			'5' : 'drudge step5',
+			'5' : 'Team Work or Slow To Die',
 			'6' : 'General Talent',
-			'7' : 'drudge step7',
+			'7' : 'Long Suffering or Steady Pace',
 			'8' : 'General Talent',
-			'9' : 'drudge step9',
-			'10' : 'drudge step10',
+			'9' : 'Long Suffering or Slow To Die',
+			'10' : 'Cant\'t Keep Me Down',
 		}
 	},
 	'graverobber' : {
-		type:'Basic',
 		name: 'graverobber',
 		text:'graverobber text',
 		gear:'Grimoire: 1 Necromancy Magia, 1 Enchanting Magia, 3 Immuto. Magical Theory',
 		talent:{
 			name:'black soul',
-			text:'Whenever this character...graverobber'
+			text:'When failed a Magic duel during dramatic time, may draw a card. Epilogue: may advance in any Magical skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'graverobber step1',
+			'1' : 'Morbid Thoughts',
 			'2' : 'General Talent',
-			'3' : 'graverobber step3',
+			'3' : 'Deathly Pallor or Mastered Immuto',
 			'4' : 'General Talent',
-			'5' : 'graverobber step5',
+			'5' : 'Deathly Pallor or Mastered Magia',
 			'6' : 'General Talent',
-			'7' : 'graverobber step7',
+			'7' : 'Macabre Infusions or Mastered Immuto',
 			'8' : 'General Talent',
-			'9' : 'graverobber step9',
-			'10' : 'graverobber step10',
+			'9' : 'Macabre Infusions or Mastered Magia',
+			'10' : 'Unholy Beacon',
 		}
 	},
 	'guard' : {
-		type:'Basic',
 		name: 'guard',
 		text:'guard text',
 		gear:'melee weapon, armor (pg 172,176) of a combined value up to 25§',
 		talent:{
 			name:'now you are pissing me off',
-			text:'Whenever this character...guard'
+			text:'When failed a Close Combat duel during dramatic time, may draw a card. Epilogue: may advance in any Close Combat skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'guard step1',
+			'1' : 'Stoic Defender',
 			'2' : 'General Talent',
-			'3' : 'guard step3',
+			'3' : 'Inevitable Defence or Take The Hit',
 			'4' : 'General Talent',
-			'5' : 'guard step5',
+			'5' : 'Inevitable Defence or Hard To Wound',
 			'6' : 'General Talent',
-			'7' : 'guard step7',
+			'7' : 'Hard To Wound or Flurry',
 			'8' : 'General Talent',
-			'9' : 'guard step9',
-			'10' : 'guard step10',
+			'9' : 'Flurry or Take The Hit',
+			'10' : 'Unyielding',
 		}
 	},
 	'gunfighter' : {
-		type:'Basic',
 		name: 'gunfighter',
 		text:'gunfighter text',
-		gear:'pistol or shotgun of a value up to 20§. This weapon gets 1 customization (pg 150) at no additional cost',
+		gear:'pistol or shotgun (20§). This weapon gets 1 customization (pg 150) at no additional cost',
 		talent:{
 			name:'finger on the trigger',
-			text:'Whenever this character...gunfighter'
+			text:'When failed a Ranged Combat duel during dramatic time, may draw a card. Epilogue: may advance in any Ranged Combat skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'gunfighter step1',
+			'1' : 'Quick Draw',
 			'2' : 'General Talent',
-			'3' : 'gunfighter step3',
+			'3' : 'Call Out or Gunfighter',
 			'4' : 'General Talent',
-			'5' : 'gunfighter step5',
+			'5' : 'Call Out or No Time For This',
 			'6' : 'General Talent',
-			'7' : 'gunfighter step7',
+			'7' : 'No Time For This or Rapid Fire',
 			'8' : 'General Talent',
-			'9' : 'gunfighter step9',
-			'10' : 'gunfighter step10',
+			'9' : 'Gunfighter or Rapid Fire',
+			'10' : 'Ranged Expert',
 		}
 	},
 	'mercenary' : {
-		type:'Basic',
 		name: 'mercenary',
 		text:'mercenary text',
 		gear:'carabine or rifle of a value up to 20§. This weapon gets 1 customization (pg 150) at no additional cost',
 		talent:{
 			name:'deadset',
-			text:'Whenever this character...mercenary'
+			text:'When failed a Ranged Combat duel during dramatic time, may draw a card. Epilogue: may advance in any Ranged Combat skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'mercenary step1',
+			'1' : 'Speed Loading',
 			'2' : 'General Talent',
-			'3' : 'mercenary step3',
+			'3' : 'Gruesome Attack or Tricky Shot',
 			'4' : 'General Talent',
-			'5' : 'mercenary step5',
+			'5' : 'Coin For Kills',
 			'6' : 'General Talent',
-			'7' : 'mercenary step7',
+			'7' : 'Relentless or Tricky Shot',
 			'8' : 'General Talent',
-			'9' : 'mercenary step9',
-			'10' : 'mercenary step10',
+			'9' : 'Relentless or Gruesome Attack',
+			'10' : 'Endless Pursuit',
 		}
 	},
 	'overseer' : {
-		type:'Basic',
 		name: 'overseer',
 		text:'overseer text',
 		gear:'non-magical skill toolkit (pg 179)',
 		talent:{
 			name:'oversight',
-			text:'Whenever this character...overseer'
+			text:'Whenever failed an Social duel, may draw a card. Epilogue: may advance in any Social skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'overseer step1',
+			'1' : 'Converted Effort',
 			'2' : 'General Talent',
-			'3' : 'overseer step3',
+			'3' : 'Time Is Money or Work Smarter',
 			'4' : 'General Talent',
-			'5' : 'overseer step5',
+			'5' : 'Delegate or Work Smarter',
 			'6' : 'General Talent',
-			'7' : 'overseer step7',
+			'7' : 'Time Is Money or You Lazy So & So',
 			'8' : 'General Talent',
-			'9' : 'overseer step9',
-			'10' : 'overseer step10',
+			'9' : 'Delegate or You Lazy So & So',
+			'10' : 'Now Men!',
 		}
 	},
 	'performer' : {
-		type:'Basic',
 		name: 'performer',
 		text:'performer text',
 		gear:'non-magical skill toolkit (pg 179)',
 		talent:{
 			name:'flare for the dramatic',
-			text:'Whenever this character...performer'
+			text:'Whenever failed an Social duel, may draw a card. Epilogue: may advance in any Social skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'performer step1',
+			'1' : 'Powerful Impression',
 			'2' : 'General Talent',
-			'3' : 'performer step3',
+			'3' : 'Grace Under Fire or Helluva Entrance',
 			'4' : 'General Talent',
-			'5' : 'performer step5',
+			'5' : 'Classically Trained or Grace Under Fire',
 			'6' : 'General Talent',
-			'7' : 'performer step7',
+			'7' : 'Graceful Exit or Helluva Entrance',
 			'8' : 'General Talent',
-			'9' : 'performer step9',
-			'10' : 'performer step10',
+			'9' : 'Classically Trained or Graceful Exit',
+			'10' : 'That\'s Showbiz',
 		}
 	},
 	'pioneer' : {
-		type:'Basic',
 		name: 'pioneer',
 		text:'pioneer text',
 		gear:'non-magical skill toolkit (pg 179)',
 		talent:{
 			name:'rugged individual',
-			text:'Whenever this character...pioneer'
+			text:'Whenever failed an Training duel, may draw a card. Epilogue: may advance in any Training skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'pioneer step1',
+			'1' : 'Born Under A Wandering Star',
 			'2' : 'General Talent',
-			'3' : 'pioneer step3',
+			'3' : 'Circle The Wagons or Tougth as Nails',
 			'4' : 'General Talent',
-			'5' : 'pioneer step5',
+			'5' : 'Circle The Wagons or Ornery',
 			'6' : 'General Talent',
-			'7' : 'pioneer step7',
+			'7' : 'Soldier On or Tougth as Nails',
 			'8' : 'General Talent',
-			'9' : 'pioneer step9',
-			'10' : 'pioneer step10',
+			'9' : 'Ornery',
+			'10' : 'Relentless',
 		}
 	},
 	'scrapper' : {
-		type:'Basic',
 		name: 'scrapper',
 		text:'scrapper text',
 		gear:'melee weapon, armor (pg 172,176) of a combined value up to 25§',
 		talent:{
 			name:'bloody minded',
-			text:'Whenever this character...scrapper'
+			text:'When failed a Close Combat duel during dramatic time, may draw a card. Epilogue: may advance in any Close Combat skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'scrapper step1',
+			'1' : 'Close the Gap',
 			'2' : 'General Talent',
-			'3' : 'scrapper step3',
+			'3' : 'Flurry or Recovery',
 			'4' : 'General Talent',
-			'5' : 'scrapper step5',
+			'5' : 'Flurry or Watch Them All',
 			'6' : 'General Talent',
-			'7' : 'scrapper step7',
+			'7' : 'Wicked or Recovery',
 			'8' : 'General Talent',
-			'9' : 'scrapper step9',
-			'10' : 'scrapper step10',
+			'9' : 'Wicked or Watch Them All',
+			'10' : 'Melee Expert',
 		}
 	},
 	'tinkerer' : {
-		type:'Basic',
 		name: 'tinkerer',
 		text:'tinkerer text',
 		gear:'Grimoire: 1 Enchanting Magia, 1 Sorcery Magia, 3 Immuto. Magical Theory',
 		talent:{
 			name:'gear head',
-			text:'Whenever this character...tinkerer'
+			text:'When failed a Magic duel during dramatic time, may draw a card. Epilogue: may advance in any Magic skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'tinkerer step1',
+			'1' : 'Push the Limits',
 			'2' : 'General Talent',
-			'3' : 'tinkerer step3',
+			'3' : 'Combat Construct or Percussive Maintenance',
 			'4' : 'General Talent',
-			'5' : 'tinkerer step5',
+			'5' : 'Combat Construct or Unrealistic Expectations',
 			'6' : 'General Talent',
-			'7' : 'tinkerer step7',
+			'7' : 'Multi-Tasker or Percussive Maintenance',
 			'8' : 'General Talent',
-			'9' : 'tinkerer step9',
-			'10' : 'tinkerer step10',
+			'9' : 'Multi-Tasker or Unrealistic Expectations',
+			'10' : 'Over Pressure',
 		}
 	},
 	'wastrel' : {
-		type:'Basic',
 		name: 'wastrel',
 		text:'wastrel text',
 		gear:'non-magical skill toolkit (pg 179)',
 		talent:{
 			name:'educated',
-			text:'Whenever this character...wastrel'
+			text:'Whenever failed an Expertise duel, may draw a card. Epilogue: may advance in any Expertise skill in addition to skill Advancement options.'
 		},
 		step:{
-			'1' : 'wastrel step1',
+			'1' : 'Luck of the Draw',
 			'2' : 'General Talent',
-			'3' : 'wastrel step3',
+			'3' : 'Cards Up A Sleeve or Trump Card',
 			'4' : 'General Talent',
-			'5' : 'wastrel step5',
+			'5' : 'Cards Up A Sleeve or All In',
 			'6' : 'General Talent',
-			'7' : 'wastrel step7',
+			'7' : 'Play For Blood or Trump Card',
 			'8' : 'General Talent',
-			'9' : 'wastrel step9',
-			'10' : 'wastrel step10',
+			'9' : 'All In or Play For Blood',
+			'10' : 'Fifty-Fifty Chance',
 		}
 	},
 }
