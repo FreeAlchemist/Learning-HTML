273357d1073a890fa99bd0911565272521605510
FreeAlchemist <freealchemist@mail.ru>
2016-04-27 07:41:21 +0300

    Create pursuits-into-the-bayou.js

---
+++ pursuits/js/pursuits-into-the-bayou.js
@@ -0,0 +1,24 @@
+var pursuit = {
+	'bokor' : {
+		type:'Into The Bayou',
+		name: 'bokor',
+		text:'bokor text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...bokor'
+		},
+		step:{
+			'1' : 'bokor step1',
+			'2' : 'General Talent',
+			'3' : 'bokor step3',
+			'4' : 'General Talent',
+			'5' : 'bokor step5',
+			'6' : 'General Talent',
+			'7' : 'bokor step7',
+			'8' : 'General Talent',
+			'9' : 'bokor step9',
+			'10' : 'bokor step10',
+		}
+	},
+}
