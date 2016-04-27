03f6ca073fe93d49ce6fd677e5c7e687f562958f
FreeAlchemist <freealchemist@mail.ru>
2016-04-27 07:40:40 +0300

    Create pursuits-advanced.js

---
+++ pursuits/js/pursuits-advanced.js
@@ -0,0 +1,24 @@
+var pursuit = {
+	'death-marshal' : {
+		type:'Advanced',
+		name: 'death-marshal',
+		text:'death-marshal text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...death-marshal'
+		},
+		step:{
+			'1' : 'death-marshal step1',
+			'2' : 'General Talent',
+			'3' : 'death-marshal step3',
+			'4' : 'General Talent',
+			'5' : 'death-marshal step5',
+			'6' : 'General Talent',
+			'7' : 'death-marshal step7',
+			'8' : 'General Talent',
+			'9' : 'death-marshal step9',
+			'10' : 'death-marshal step10',
+		}
+	},
+}
